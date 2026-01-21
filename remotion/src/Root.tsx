66c1878a7afdd21b1e9f6e58dc847debe955e00b
matiashoyld <matiashoyl@gmail.com>
2026-01-21 10:28:05 -0800

    feat: wire up all scenes in Root.tsx with Series composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,16 +1,116 @@
-import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { Composition, Series } from "remotion";
+import { Scene1Opening } from "./scenes/Scene1Opening";
+import { Scene2CVUpload } from "./scenes/Scene2CVUpload";
+import { Scene3HRInterview } from "./scenes/Scene3HRInterview";
+import { Scene4SlackCollab } from "./scenes/Scene4SlackCollab";
+import { Scene5SubmitPR } from "./scenes/Scene5SubmitPR";
+import { Scene6Results } from "./scenes/Scene6Results";
+
+// Video configuration
+const FPS = 30;
+const WIDTH = 1920;
+const HEIGHT = 1080;
+
+// Scene durations in frames (at 30fps)
+const SCENE_DURATIONS = {
+  scene1Opening: 8 * FPS,     // 240 frames (8 seconds)
+  scene2CVUpload: 12 * FPS,   // 360 frames (12 seconds)
+  scene3HRInterview: 18 * FPS, // 540 frames (18 seconds)
+  scene4SlackCollab: 22 * FPS, // 660 frames (22 seconds)
+  scene5SubmitPR: 12 * FPS,   // 360 frames (12 seconds)
+  scene6Results: 18 * FPS,    // 540 frames (18 seconds)
+} as const;
+
+// Total duration: 90 seconds = 2700 frames
+const TOTAL_DURATION = Object.values(SCENE_DURATIONS).reduce((a, b) => a + b, 0);
+
+// Full promo video composition using Series
+const PromoVideo: React.FC = () => {
+  return (
+    <Series>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene1Opening}>
+        <Scene1Opening />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene2CVUpload}>
+        <Scene2CVUpload />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene3HRInterview}>
+        <Scene3HRInterview />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene4SlackCollab}>
+        <Scene4SlackCollab />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene5SubmitPR}>
+        <Scene5SubmitPR />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={SCENE_DURATIONS.scene6Results}>
+        <Scene6Results />
+      </Series.Sequence>
+    </Series>
+  );
+};
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* Full promo video - 90 seconds */}
       <Composition
-        id="MyComp"
-        component={MyComposition}
-        durationInFrames={60}
-        fps={30}
-        width={1280}
-        height={720}
+        id="PromoVideo"
+        component={PromoVideo}
+        durationInFrames={TOTAL_DURATION}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+
+      {/* Individual scene compositions for preview/testing */}
+      <Composition
+        id="Scene1Opening"
+        component={Scene1Opening}
+        durationInFrames={SCENE_DURATIONS.scene1Opening}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="Scene2CVUpload"
+        component={Scene2CVUpload}
+        durationInFrames={SCENE_DURATIONS.scene2CVUpload}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="Scene3HRInterview"
+        component={Scene3HRInterview}
+        durationInFrames={SCENE_DURATIONS.scene3HRInterview}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="Scene4SlackCollab"
+        component={Scene4SlackCollab}
+        durationInFrames={SCENE_DURATIONS.scene4SlackCollab}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="Scene5SubmitPR"
+        component={Scene5SubmitPR}
+        durationInFrames={SCENE_DURATIONS.scene5SubmitPR}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
+      />
+      <Composition
+        id="Scene6Results"
+        component={Scene6Results}
+        durationInFrames={SCENE_DURATIONS.scene6Results}
+        fps={FPS}
+        width={WIDTH}
+        height={HEIGHT}
       />
     </>
   );
